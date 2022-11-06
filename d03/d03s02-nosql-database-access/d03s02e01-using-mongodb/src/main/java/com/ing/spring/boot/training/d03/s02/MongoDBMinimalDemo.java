e4babd7c97d85300950a81809a7530ce788167b0
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-06 21:41:40 +0200

    [dev] Added the MongoDB sample

---
+++ d03/d03s02-nosql-database-access/d03s02e01-using-mongodb/src/main/java/com/ing/spring/boot/training/d03/s02/MongoDBMinimalDemo.java
@@ -0,0 +1,12 @@
+package com.ing.spring.boot.training.d03.s02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDBMinimalDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDBMinimalDemo.class, args);
+    }
+}
---
+++ d03/d03s02-nosql-database-access/d03s02e01-using-mongodb/src/main/java/com/ing/spring/boot/training/d03/s02/entity/Product.java
@@ -0,0 +1,77 @@
+package com.ing.spring.boot.training.d03.s02.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Document(collection = "product")
+public class Product {
+
+    @Id
+    private int id;
+    @Indexed
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Product: {");
+
+        sb.append("id: ").append(id);
+        sb.append(", name: '").append(name).append('\'');
+        sb.append(", \tprice: ").append(price);
+        sb.append('}');
+
+        return sb.toString();
+    }
+}

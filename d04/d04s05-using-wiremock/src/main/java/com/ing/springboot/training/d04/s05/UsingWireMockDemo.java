7dba20958d8643bea3e7a1277a869ae94667f010
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-09 22:33:25 +0200

    [dev] Improved the WireMock samples

---
+++ d04/d04s05-using-wiremock/src/main/java/com/ing/springboot/training/d04/s05/UsingWireMockDemo.java
@@ -0,0 +1,12 @@
+package com.ing.springboot.training.d04.s05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingWireMockDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingWireMockDemo.class, args);
+    }
+}
--- d04/d04s05-using-wiremock/src/test/java/com/ing/springboot/training/d04/s05/ProgrammaticallyManagedTest.java
+++ d04/d04s05-using-wiremock/src/test/java/com/ing/springboot/training/d04/s05/ProgrammaticallyManagedTest.java
@@ -5,12 +5,11 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.Test;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
@@ -19,37 +18,39 @@ import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ProgrammaticallyManagedTest {
 
     private static final String TESTED_PATH = "/spring-boot";
 
-    private final WireMockServer wireMockServer = new WireMockServer();
-    private final CloseableHttpClient httpClient = HttpClients.createDefault();
 
     @Test
-    public void givenProgrammaticallyManagedServer_whenUsingSimpleStubbing_thenCorrect() throws IOException {
+    public void givenAProgrammaticallyManagedWireMockServer_whenUsingSimpleStubbing_thenTheResponseIsCorrect() throws IOException {
+        final WireMockServer wireMockServer = new WireMockServer();
         wireMockServer.start();
 
         configureFor("localhost", 8080);
         stubFor(get(urlEqualTo(TESTED_PATH)).willReturn(aResponse().withBody("Welcome to WireMock!")));
 
         HttpGet request = new HttpGet("http://localhost:8080/spring-boot");
-        HttpResponse httpResponse = httpClient.execute(request);
-        String stringResponse = convertResponseToString(httpResponse);
+        HttpResponse httpResponse;
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            httpResponse = httpClient.execute(request);
+            String stringResponse = convertResponseToString(httpResponse);
 
-        verify(getRequestedFor(urlEqualTo(TESTED_PATH)));
-        assertEquals("Welcome to spring-boot!", stringResponse);
+            verify(getRequestedFor(urlEqualTo(TESTED_PATH)));
+            assertEquals("Welcome to WireMock!", stringResponse);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
 
         wireMockServer.stop();
     }
 
-    private static String convertResponseToString(HttpResponse response) throws IOException {
-        InputStream responseStream = response.getEntity().getContent();
-        Scanner scanner = new Scanner(responseStream, StandardCharsets.UTF_8);
-        String stringResponse = scanner.useDelimiter("\\Z").next();
-        scanner.close();
-        return stringResponse;
+    private static String convertResponseToString(HttpResponse httpResponse) throws IOException {
+        return EntityUtils.toString(httpResponse.getEntity(), StandardCharsets.UTF_8);
     }
 }

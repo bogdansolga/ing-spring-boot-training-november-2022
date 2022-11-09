9bc3fbd19f2c8933df7ec67c9b72ef86880c2a2c
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-09 08:25:17 +0200

    [dev] Use an Optional for the Autowired field

--- d01/d01s04-component-scanning-and-qualifying/d01s04e02-component-model/src/main/java/com/ing/springboot/training/d01/s04/service/ProductProcessor.java
+++ d01/d01s04-component-scanning-and-qualifying/d01s04e02-component-model/src/main/java/com/ing/springboot/training/d01/s04/service/ProductProcessor.java
@@ -4,9 +4,11 @@ import com.ing.springboot.training.d01.s04.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class ProductProcessor {
 
     @Autowired
-    private ProductRepository productRepository;
+    private Optional<ProductRepository> productRepository;
 }

da7fa68d688dfc011541428e43c5e7dfa6f02406
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-10 09:04:02 +0200

    [dev] Create the keyspace on startup

--- d04/d04s06-using-test-containers/src/test/java/com/ing/springboot/training/d04/s06/SampleCassandraTest.java
+++ d04/d04s06-using-test-containers/src/test/java/com/ing/springboot/training/d04/s06/SampleCassandraTest.java
@@ -1,5 +1,7 @@
 package com.ing.springboot.training.d04.s06;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,15 +14,35 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Testcontainers
 public class SampleCassandraTest {
 
+    private static final int PORT = 9042;
+
+    private static final String KEYSPACE_NAME = "spring_boot_cassandra_demo";
+
     @Container
     private static final CassandraContainer<?> CASSANDRA_CONTAINER =
-            (CassandraContainer) new CassandraContainer("cassandra:4.1").withExposedPorts(9042);
+            (CassandraContainer) new CassandraContainer("cassandra:4.1").withExposedPorts(PORT);
 
     @BeforeAll
     public static void setupCassandraConnectionProperties() {
-        System.setProperty("spring.data.cassandra.keyspace-name", "spring_boot_cassandra_demo");
+        System.setProperty("spring.data.cassandra.keyspace-name", KEYSPACE_NAME);
         System.setProperty("spring.data.cassandra.port",
-                String.valueOf(CASSANDRA_CONTAINER.getMappedPort(9042)));
+                String.valueOf(CASSANDRA_CONTAINER.getMappedPort(PORT)));
+
+        try (Cluster cluster = createCluster();
+             Session session = cluster.connect()) {
+            session.execute("CREATE KEYSPACE IF NOT EXISTS " + KEYSPACE_NAME +
+                    " WITH replication = \n" +
+                    "{'class':'SimpleStrategy','replication_factor':'1'};");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static Cluster createCluster() {
+        return Cluster.builder()
+                      .addContactPoint(CASSANDRA_CONTAINER.getHost())
+                      .withPort(CASSANDRA_CONTAINER.getMappedPort(PORT))
+                      .build();
     }
 
     @Test

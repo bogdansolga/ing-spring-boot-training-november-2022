267ae32002c3966c4f4f74f425d757172a17d88f
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-10 15:57:36 +0200

    [dev] Added an operation

--- d03/d03s02-nosql-database-access/d03s02e01-using-cassandra/src/main/java/com/ing/spring/boot/training/d03/s02/repository/ProductRepository.java
+++ d03/d03s02-nosql-database-access/d03s02e01-using-cassandra/src/main/java/com/ing/spring/boot/training/d03/s02/repository/ProductRepository.java
@@ -2,6 +2,13 @@ package com.ing.spring.boot.training.d03.s02.repository;
 
 import com.ing.spring.boot.training.d03.s02.entity.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    Optional<List<Product>> findAllByName(String name);
 }

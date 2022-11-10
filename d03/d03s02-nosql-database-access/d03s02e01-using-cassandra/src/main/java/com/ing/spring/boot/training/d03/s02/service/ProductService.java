c128c9aad83d05fa912219223808c912f489f209
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-10 15:57:00 +0200

    [improve] Added the CassandraTemplate

--- d03/d03s02-nosql-database-access/d03s02e01-using-cassandra/src/main/java/com/ing/spring/boot/training/d03/s02/service/ProductService.java
+++ d03/d03s02-nosql-database-access/d03s02e01-using-cassandra/src/main/java/com/ing/spring/boot/training/d03/s02/service/ProductService.java
@@ -5,6 +5,7 @@ import com.ing.spring.boot.training.d03.s02.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,10 +18,13 @@ import java.util.stream.StreamSupport;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CassandraTemplate cassandraTemplate;
 
     @Autowired
-    public ProductService(final ProductRepository productRepository) {
+    public ProductService(final ProductRepository productRepository,
+                          CassandraTemplate cassandraTemplate) {
         this.productRepository = productRepository;
+        this.cassandraTemplate = cassandraTemplate;
     }
 
     @Async
@@ -34,5 +38,10 @@ public class ProductService {
     public List<Product> getAll() {
         return StreamSupport.stream(productRepository.findAll().spliterator(), false)
                             .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public long count() {
+        return cassandraTemplate.count(Product.class);
     }
 }

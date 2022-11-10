ec7794910c3313c2193059f2bfb55ca915055977
Bogdan Solga <bogdan.solga@gmail.com>
2022-11-10 15:58:31 +0200

    [improve] Added the MongoTemplate

--- d03/d03s02-nosql-database-access/d03s02e01-using-mongodb/src/main/java/com/ing/spring/boot/training/d03/s02/service/ProductService.java
+++ d03/d03s02-nosql-database-access/d03s02e01-using-mongodb/src/main/java/com/ing/spring/boot/training/d03/s02/service/ProductService.java
@@ -5,11 +5,14 @@ import com.ing.spring.boot.training.d03.s02.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -17,10 +20,13 @@ import java.util.stream.StreamSupport;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public ProductService(final ProductRepository productRepository) {
+    public ProductService(final ProductRepository productRepository,
+                          final MongoTemplate mongoTemplate) {
         this.productRepository = productRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     @Async
@@ -34,5 +40,11 @@ public class ProductService {
     public List<Product> getAll() {
         return StreamSupport.stream(productRepository.findAll().spliterator(), false)
                             .collect(Collectors.toList());
+    }
+
+    @PostConstruct
+    public void display() {
+        final Set<String> collectionNames = mongoTemplate.getCollectionNames();
+        collectionNames.forEach(System.out::println);
     }
 }
